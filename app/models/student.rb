92c7f4530e9edbcfd8ad19dfb26d94d2d2eb0e24
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-29 16:58:34 -0500

    Corrects rspec tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,15 @@
 require_relative '../../db/config'
+require 'time'
+require 'date'
 
-# implement your Student model here
+class Student < ActiveRecord::Base
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def age
+    d = self.birthday
+    t = Time.now
+    today_convert - birth_convert
+  end
+end
